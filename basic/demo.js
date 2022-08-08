e9e5b4ea869257193e619d0e4f0657cac5e5b982
ravindrakumar2907 <ravindramore6@gmail.com>
2022-08-08 21:01:40 +0530

    This is my first file

---
+++ basic/demo.js
@@ -0,0 +1,31 @@
+console.log("This is node js demo");
+
+var name = "ravi"; // initilisation 
+var name1; /// declare
+
+var num1 = 10;
+var num2 = 20;
+var result;
+
+result = num1 + num2;
+console.log("The result of num1 and num2 is: " + result);
+
+result = num1 - num2;
+console.log("The result of num1 and num2 is: " + result);
+
+result = num1 * num2;
+console.log("The result of num1 and num2 is: " + result);
+
+result = num1 / num2;
+console.log("The result of num1 and num2 is: " + result);
+
+result = num1 % num2;
+var print =  "The result of num1 and num2 is: " + result;
+console.log(print);
+
+
+//http://localhost:80/login
+
+//http://ip/login
+
+//github
