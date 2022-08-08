1f16c78d56731888fe8f4535774dc8d362a8d4f5
ravindrakumar2907 <ravindramore6@gmail.com>
2022-08-09 01:12:11 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const url = require('url');
+
+const hostName = "0.0.0.0";
+const port = 3000;
+
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
+});
+
+server.listen(port, host, () =>{
+    console.log("server started running on http://{hostName}:{port}/");
+});
