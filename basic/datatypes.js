7a3b1f88f14c22577ba76853361a7901f7fda200
ravindrakumar2907 <ravindramore6@gmail.com>
2022-08-09 21:14:03 +0530

    added datatypes file

---
+++ basic/datatypes.js
@@ -0,0 +1,34 @@
+
+var name = "ravi";
+console.log("This is datatype demo: " + typeof name);
+
+var dob = "DD/MM/YYYY";
+var dateOfBirth = {day:"",
+mm: "",
+yy:""
+}
+
+var user = {
+    name:"",
+    email: "",
+    dob:"",
+    mobile_number: ""
+}
+console.log("object: " + typeof user);
+
+
+var x = 16 + " " + 16 ;
+console.log(x);
+console.log("String: " + typeof x);
+console.log("Number: " + typeof 1);
+//Number 1
+// String "" ''
+//Object {}
+//Array [1,2]
+//Boolean true / false
+//undefied
+var demo;
+var demo1;
+console.log("undefined: " + typeof demo);
+var result1 = demo + demo1;
+console.log(result1)
